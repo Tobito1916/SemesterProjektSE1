8034f61b115d8193407bea4cc37d3ae512dbd898
patrick <patrick@bequalified.de>
2017-10-23 11:47:46 +0200

    added basetest

---
+++ src/de/hbrs/se1/parkhaus/main.java
@@ -0,0 +1,14 @@
+package de.hbrs.se1.parkhaus;
+
+import org.junit.Test;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("bla");
+	}
+
+	@Test
+	public void test() {
+		System.out.println("bla fuu");
+	}
+}
