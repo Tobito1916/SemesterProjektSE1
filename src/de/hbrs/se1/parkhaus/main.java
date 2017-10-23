94b473930aad71c3e02d0c5ac30e2a41f42412d0
patrick <patrick@bequalified.de>
2017-10-23 12:01:44 +0200

    clean up main.java

--- src/de/hbrs/se1/parkhaus/main.java
+++ src/de/hbrs/se1/parkhaus/main.java
@@ -1,19 +1,7 @@
 package de.hbrs.se1.parkhaus;
 
-import org.junit.Test;
-
 public class main {
 	public static void main(String[] args) {
-		System.out.println("bla");
-	}
-
-	@Test
-	public void test() {
-		System.out.println("bla fuu");
-	}
-	
-	@Test
-	public void test2() {
-		System.out.println("Test");
+		System.out.println("Hallo Welt.");
 	}
 }
